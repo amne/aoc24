83455b22117f7805b65904a79cd8e6b75ea54722
Cornel Cruceru <amne@amne.me>
2024-12-12 10:11:57 +0200

    fixed dap config

---
+++ .nvim-dap.lua
@@ -0,0 +1,21 @@
+local dap = require("dap")
+
+
+dap.adapters.python = {
+    type = 'executable',
+    command = 'python',
+    args = { '-m', 'debugpy.adapter' }
+}
+
+dap.configurations.python = {
+    {
+        type = 'python',
+        request = 'launch',
+        name = "Launch file",
+        program = "${file}",
+        pythonPath = function()
+            return '/usr/bin/python3'
+        end,
+    }
+}
+
