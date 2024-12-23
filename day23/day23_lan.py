d54c14ae9a9bd92593c11f1623f43916fcc43edf
cornel cruceru <amne@amne.me>
2024-12-23 22:19:36 +0200

    day23 p1 faster now

--- day23/day23_lan.py
+++ day23/day23_lan.py
@@ -32,6 +32,25 @@ def find_n3(computer_links, connections, match_1st = 't'):
     # [print(g) for g in games]
     print(len(list(filter(lambda x: [c[0] == match_1st for c in x].count(True) > 0, games))))
 
+def find_n3_2(computer_links, connections, match_1st = 't'):
+    networks = set() 
+    for computer in sorted(computer_links.keys()):
+        for network in connections:
+            if computer < network[1]:
+                continue
+            connected = True
+            for c in network:
+                if computer not in computer_links[c]:
+                    connected = False
+                    break
+            if connected:
+                if 't' in network[0][0] + network[1][0] + computer[0]:
+                    networks.add((network[0], network[1], computer))
+    print(len(networks))
+    # [print(n) for n in networks]
+    
+
+
 def find_max_network(computer_links, connections):
     # some of the sorts got left behind from a failed attempt to solve this problem
     for computer, computer_network in computer_links.items():
@@ -56,7 +75,8 @@ def find_max_network(computer_links, connections):
 
 
 def find_t(computer_links, connections):
-    find_n3(computer_links, connections)
+    find_n3_2(computer_links, connections) # much faster than find_n3
+    # find_n3(computer_links, connections)
     find_max_network(computer_links, connections)
     return
 
